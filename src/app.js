50017488e65aff3681800812bbb89e9615be55f2
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-07 10:49:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import { renderHeader } from "./ui/header.js";
+import { renderTabs } from "./ui/tabs.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   renderHeader();
+  renderTabs();
 });
